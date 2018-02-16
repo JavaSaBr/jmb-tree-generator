7f8fc487a7d79b9d877a865fe443899e60d312c3
JavaSaBr <javasabr@gmail.com>
2018-02-16 11:48:33 +0300

    fixed plugin for 1.7.0

--- src/main/java/com/ss/editor/tree/generator/editor/TreeGeneratorEditorState.java
+++ src/main/java/com/ss/editor/tree/generator/editor/TreeGeneratorEditorState.java
@@ -1,6 +1,9 @@
 package com.ss.editor.tree.generator.editor;
 
 import com.ss.editor.annotation.FxThread;
+import com.ss.editor.config.DefaultSettingsProvider;
+import com.ss.editor.config.DefaultSettingsProvider.Defaults;
+import com.ss.editor.config.DefaultSettingsProvider.Preferences;
 import com.ss.editor.ui.component.editor.state.impl.Editor3DWithEditorToolEditorState;
 
 /**
@@ -22,7 +25,7 @@ public class TreeGeneratorEditorState extends Editor3DWithEditorToolEditorState 
 
     public TreeGeneratorEditorState() {
         this.cameraLocation.set(0, 2, 0);
-        this.enableLight = EDITOR_CONFIG.isDefaultEditorCameraEnabled();
+        this.enableLight = EDITOR_CONFIG.getBoolean(Preferences.PREF_CAMERA_LAMP, Defaults.PREF_DEFAULT_CAMERA_LIGHT);
     }
 
     /**
