1143c424133441cf6c9f2a9e967d28e0f62043f1
Alex Brui <javasabr@gmail.com>
2018-02-19 08:37:15 +0300

    fixed reading editor config.

--- src/main/java/com/ss/editor/tree/generator/editor/TreeGeneratorEditorState.java
+++ src/main/java/com/ss/editor/tree/generator/editor/TreeGeneratorEditorState.java
@@ -1,5 +1,7 @@
 package com.ss.editor.tree.generator.editor;
 
+import static com.ss.editor.config.DefaultSettingsProvider.Defaults.PREF_DEFAULT_CAMERA_LIGHT;
+import static com.ss.editor.config.DefaultSettingsProvider.Preferences.PREF_CAMERA_LAMP;
 import com.ss.editor.annotation.FxThread;
 import com.ss.editor.ui.component.editor.state.impl.Editor3DWithEditorToolEditorState;
 
@@ -22,7 +24,7 @@ public class TreeGeneratorEditorState extends Editor3DWithEditorToolEditorState 
 
     public TreeGeneratorEditorState() {
         this.cameraLocation.set(0, 2, 0);
-        this.enableLight = EDITOR_CONFIG.isDefaultEditorCameraEnabled();
+        this.enableLight = EDITOR_CONFIG.getBoolean(PREF_CAMERA_LAMP, PREF_DEFAULT_CAMERA_LIGHT);
     }
 
     /**
